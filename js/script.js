324e44a2c359fc687f95d3e6e8f13d19c6b3d18d
Thomas Perez <thomasperez@uagateway.org>
2018-04-25 22:48:07 +0000

    Implemented Input and API structure

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 /* global $ */
+// Java script linked to index.html (working) //
 
-console.log('hi');
 
-$('h1').click(function(){
-    $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
+// functions
+function createUrl(inputOne,inputTwo){
+    //if (inputOne === "")
+}
+
+function callLocationByName(input1,input2) {
+    // input one is city name and input 2 is country code. *IMPORTANT*
+    var weatherAPI = "api.openweathermap.org/data/2.5/weather?q=" + input1 + "," + input2 + "&APPID=ecd07628b63a19fb3ddd1e660cbefa8f";
+    // API search link is provided in weatherAPI variable. 
+    $.ajax({
+        url: weatherAPI,
+        method: "GET",
+        success: function(response) {
+            
+        },
+    });
+}
+
+
+// call out functions //
+$(document).ready(function(){
+    //calllocationbyname(Chicago, us);
+
+    
 });
